f49c4f76b38451b4e5452b4474b9893522ad5863
Brandon Kish <bkish@schoolcraft.edu>
2023-01-16 15:11:39 -0500

    Basic form field server side validation with a quick exploration of how to nest in Action return data

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,6 +1,6 @@
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import type { ActionFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
 
 const inputClassName =
@@ -18,34 +18,65 @@ export const action: ActionFunction = async ({ request }) => {
 
   // Quick exploration of how to console log formData
   // https://stackoverflow.com/a/45673708
-  console.log(JSON.stringify(formData)); // only {}
-  console.log(...formData); // arrays of what is included in pairs
-  console.table(Object.fromEntries(formData)); // pretty table if all unique
-  console.log(formData.get("title"));
+  // console.log(JSON.stringify(formData)); // only {}
+  // console.log(...formData); // arrays of what is included in pairs
+  // console.table(Object.fromEntries(formData)); // pretty table if all unique
+  // console.log(formData.get("title"));
 
   const title = formData.get("title");
   const slug = formData.get("slug");
   const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+  if (hasErrors) {
+    // Kent in egghead.io
+    return json(errors);
+    // Brandon considering sending more back in the paylod
+    // return json({ errors });
+  }
+
   await createPost({ title, slug, markdown });
   return redirect("/posts/admin");
 };
+
 export default function NewPostRoute() {
+  // Kent in eggheadio
+  const errors = useActionData();
+  // const data = useActionData();
+  // Brandon considering sending more back in the paylod
+  // const { errors } = data || {};
   return (
-    <Form action="." method="post">
+    <Form action="/posts/admin/new" method="post">
       <p>
         <label>
-          Post Title:
+          Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
           <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
           Post Slug:
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
-        <label htmlFor="markdown"></label>
+        <label htmlFor="markdown">
+          Markdown:{" "}
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
+        </label>
         <textarea
           name="markdown"
           id="markdown"
