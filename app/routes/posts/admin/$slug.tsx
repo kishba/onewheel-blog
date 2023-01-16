17ff671dc213e236555132811dbd38d128f23a40
Brandon Kish <bkish@schoolcraft.edu>
2023-01-16 17:41:54 -0500

    Add button for deleting with new intent that deletes with the prisma client

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -7,7 +7,12 @@ import {
 } from "@remix-run/react";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { redirect, json } from "@remix-run/node";
-import { createPost, getPost, updatePost } from "~/models/post.server";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  updatePost,
+} from "~/models/post.server";
 import invariant from "tiny-invariant";
 import { requireAdminUser } from "~/session.server";
 
@@ -38,6 +43,12 @@ export const action: ActionFunction = async ({ request, params }) => {
   // });
   // console.log(await request.formData());
   const formData = await request.formData();
+  const intent = formData.get("intent");
+
+  if (intent === "delete") {
+    await deletePost(params.slug);
+    return redirect("/posts/admin");
+  }
 
   // Quick exploration of how to console log formData
   // https://stackoverflow.com/a/45673708
@@ -90,6 +101,7 @@ export default function NewPostRoute() {
   // transition.submission?.action
   const isCreating = transition.submission?.formData.get("intent") === "create";
   const isUpdating = transition.submission?.formData.get("intent") === "update";
+  const isDeleting = transition.submission?.formData.get("intent") === "delete";
   const isNewPost = !data.post;
 
   return (
@@ -137,7 +149,18 @@ export default function NewPostRoute() {
           defaultValue={data.post?.markdown}
         ></textarea>
       </p>
-      <p className="text-right">
+      <div className="flex justify-end gap-4">
+        {isNewPost ? null : (
+          <button
+            type="submit"
+            name="intent"
+            value="delete"
+            className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400 disabled:bg-red-300"
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </button>
+        )}
         <button
           type="submit"
           name="intent"
@@ -148,7 +171,7 @@ export default function NewPostRoute() {
           {isNewPost ? (isCreating ? "Creating..." : "Create Post") : null}
           {isNewPost ? null : isUpdating ? "Updating..." : "Update"}
         </button>
-      </p>
+      </div>
     </Form>
   );
 }
