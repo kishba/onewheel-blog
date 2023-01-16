b0beb7344f275da35e42451c6360a0978e591bbe
Brandon Kish <bkish@schoolcraft.edu>
2023-01-16 10:28:34 -0500

    define a type for LoaderData that resolves to the promise's actual data leading to auto complete when rendering the UI

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,7 +1,10 @@
 export async function getPosts() {
   const posts = [
     { slug: "my-first-post", title: "My First Post!" },
-    { slug: "trail-riding-with-onewheel", title: "Trail Riding with Onewheel" },
+    {
+      slug: "trail-riding-with-onewheel",
+      title: "Trail Riding with Onewheel",
+    },
   ];
   return posts;
 }
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,13 +1,18 @@
 import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPosts } from "~/models/post.server";
 
-export const loader = async () => {
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader: LoaderFunction = async () => {
   const posts = await getPosts();
   return json({ posts });
 };
 export default function PostsRoute() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as LoaderData;
   return (
     <main>
       <h1>Posts</h1>
