1922b7c2d7ef44e9ead28937bfca4725685ecb3f
Brandon Kish <bkish@schoolcraft.edu>
2023-01-16 10:19:49 -0500

    Initial commit with link to basic hardcoded route

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,32 @@
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const posts = [
+    { slug: "my-first-post", title: "My First Post!" },
+    { slug: "trail-riding-with-onewheel", title: "Trail Riding with Onewheel" },
+  ];
+
+  const postsString = JSON.stringify({ posts });
+  return new Response(postsString, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+export default function PostsRoute() {
+  const { posts } = useLoaderData();
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
